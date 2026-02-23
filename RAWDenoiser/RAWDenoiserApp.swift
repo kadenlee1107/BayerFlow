a7ebe396ea31eedc03c731306a06e99b292100e5
Kaden Lee <kadenlee1107@gmail.com>
2026-02-22 20:21:57 -0800

    Initial Commit

---
+++ RAWDenoiser/RAWDenoiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAWDenoiserApp.swift
+//  RAWDenoiser
+//
+//  Created by Kaden Lee on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct RAWDenoiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
